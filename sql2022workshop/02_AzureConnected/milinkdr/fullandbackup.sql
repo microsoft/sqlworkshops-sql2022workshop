119224c61d9df8268f31eda1f4586b9ae83b9929
Bob Ward <bobward@microsoft.com>
2022-09-06 01:04:13 -0500

    Add in scripts for cloud conneced

--- sql2022workshop/02_AzureConnected/milinkdr/fullandbackup.sql
+++ sql2022workshop/02_AzureConnected/milinkdr/fullandbackup.sql
@@ -2,7 +2,6 @@
 -- Set full recovery mode for all databases you want to replicate.
 ALTER DATABASE WideWorldImporters SET RECOVERY FULL;
 GO
-
 -- Execute backup for all databases you want to replicate.
 BACKUP DATABASE WideWorldImporters TO DISK = N'c:\sql_sample_databases\wwi.bak';
 GO
