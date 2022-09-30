9cfbe6e296e2d87fa484e64cd169f6692fb3f2e5
Bob Ward <bobward@microsoft.com>
2022-09-29 22:31:01 -0400

    Update gapfilling.sql

--- sql2022workshop/06_TSQL/timeseries/gapfilling.sql
+++ sql2022workshop/06_TSQL/timeseries/gapfilling.sql
@@ -1,14 +1,10 @@
--- Use FIRST_VALUE and LAST_VALUE to fill in gaps
+-- Use LAST_VALUE to fill in gaps
 USE GapFilling;
 GO
-SELECT timestamp
-	   , VoltageReading
-	   , FIRST_VALUE (VoltageReading) IGNORE NULLS OVER (
-			ORDER BY timestamp ASC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING 
-			) AS [FIRST_VALUE]
-		, LAST_VALUE (VoltageReading) IGNORE NULLS OVER (
-			ORDER BY timestamp DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW 
-			) AS [LAST_VALUE]
+SELECT timestamp,
+	    VoltageReading,
+		LAST_VALUE (VoltageReading) IGNORE NULLS OVER
+		(ORDER BY timestamp DESC) AS [LAST_VALUE]
 FROM MachineTelemetry
 ORDER BY [timestamp];
 GO
