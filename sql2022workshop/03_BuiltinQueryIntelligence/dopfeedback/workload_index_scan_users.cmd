6262b5276f205b3ff829d0c2f6583d5997a3b9d0
Bob Ward <bobward@microsoft.com>
2022-11-07 17:39:57 -0600

    Added trace flag
    Added -T146 for ostress to be compat with SQL22

--- sql2022workshop/03_BuiltinQueryIntelligence/dopfeedback/workload_index_scan_users.cmd
+++ sql2022workshop/03_BuiltinQueryIntelligence/dopfeedback/workload_index_scan_users.cmd
@@ -1,2 +1,2 @@
-"c:\Program Files\Microsoft Corporation\RMLUtils\ostress" -E -Q"EXEC Warehouse.GetStockItemsbySupplier 4;" -n1 -r75 -q -oworkload_wwi_regress -dWideWorldImporters
+"c:\Program Files\Microsoft Corporation\RMLUtils\ostress" -E -Q"EXEC Warehouse.GetStockItemsbySupplier 4;" -n1 -r75 -q -oworkload_wwi_regress -dWideWorldImporters -T146
 
