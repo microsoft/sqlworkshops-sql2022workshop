7c9daac614a48965c75f4686f53cccf397e786e7
Bob Ward <bobward@microsoft.com>
2022-11-09 17:37:29 -0600

    Update checkstatus.sql

--- sql2022workshop/02_AzureConnected/milinkdr/checkstatus.sql
+++ sql2022workshop/02_AzureConnected/milinkdr/checkstatus.sql
@@ -1,4 +1,7 @@
-SELECT @@SERVERNAME;
+SELECT @@SERVERNAME AS SERVERNAME, CASE   
+      WHEN SERVERPROPERTY('EngineEdition') < 5 THEN 'SQL Server'
+	  WHEN SERVERPROPERTY('EngineEdition') = 8 THEN 'Azure SQL Mananaged Instance'
+END   
 GO
 SELECT DATABASEPROPERTYEX('WideWorldImporters', 'Updateability');
 GO
