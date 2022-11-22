9bde60c67454433260553ef3b3e32db48d53765f
Bob Ward <bobward@microsoft.com>
2022-11-22 12:00:29 -0600

    Fixed issue #12

--- sql2022workshop/03_BuiltinQueryIntelligence/pspopt/query_store_dispatcher_plan.sql
+++ sql2022workshop/03_BuiltinQueryIntelligence/pspopt/query_store_dispatcher_plan.sql
@@ -2,7 +2,7 @@ USE WideWorldImporters;
 GO
 -- Look at the dispatcher plan
 -- If you "click" on the SHOWPLAN XML output you will see a "multiple plans" operator
-SELECT qp.plan_id, qp.query_plan_hash, cast (qp.query_plan as XML)
+SELECT qp.plan_id, qp.query_plan_hash, qv.query_variant_query_id, cast (qp.query_plan as XML) as dispatcher_plan
 FROM sys.query_store_plan qp
 JOIN sys.query_store_query_variant qv
 ON qp.plan_id = qv.dispatcher_plan_id;
